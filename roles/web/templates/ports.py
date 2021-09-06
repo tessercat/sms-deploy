9af0298ccb2cbd0b7ce7e973c2d40144632dee62
Peter Christensen <tessercat@pakaa.net>
2021-09-06 09:56:21 -0600

    Remove verto.

--- roles/web/templates/ports.py
+++ roles/web/templates/ports.py
@@ -2,6 +2,4 @@
     'firewall': {{ firewall_port }},
     'rtp_start': {{ freeswitch_rtp_port_start }},
     'rtp_end': {{ freeswitch_rtp_port_end }},
-    'stun': {{ stun_port }},
-    'verto': {{ freeswitch_verto_port }},
 }
